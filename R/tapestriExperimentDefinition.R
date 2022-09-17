3b760c59ff69eda54ed1d30098924ae62e4a85e3
Joey Mays <joeymays94@gmail.com>
2022-09-17 19:24:56 -0400

    show method for TapestriExperiment, custom slot getters and setters

--- R/tapestriExperimentDefinition.R
+++ R/tapestriExperimentDefinition.R
@@ -16,3 +16,55 @@
     slots = c(barcodeProbe = "character",
               grnaProbe = "character")
 )
+
+
+#' @describeIn .TapestriExperiment Show method for Tapestri Experiment
+#' @param TapestriExperiment A TapestriExperiment object
+#' @param object An R object
+#'
+#' @export
+#' @importMethodsFrom SingleCellExperiment show
+setMethod("show", "TapestriExperiment", function(object) {
+    callNextMethod()
+    cat(
+        "barcodeProbe: ", object@barcodeProbe, "\n",
+        "grnaProbe: ", object@grnaProbe,
+        sep = ""
+    )
+})
+
+setGeneric("barcodeProbe", function(x) standardGeneric("barcodeProbe"))
+setMethod("barcodeProbe", "TapestriExperiment", function(x) x@barcodeProbe)
+setGeneric("barcodeProbe<-", function(x, value) standardGeneric("barcodeProbe<-"))
+setMethod("barcodeProbe<-", "TapestriExperiment", function(x, value) {
+    x@barcodeProbe <- value
+    validObject(x)
+    x
+})
+
+setGeneric("grnaProbe", function(x) standardGeneric("grnaProbe"))
+setMethod("grnaProbe", "TapestriExperiment", function(x) x@grnaProbe)
+setGeneric("grnaProbe<-", function(x, value) standardGeneric("grnaProbe<-"))
+setMethod("grnaProbe<-", "TapestriExperiment", function(x, value) {
+    x@grnaProbe <- value
+    validObject(x)
+    x
+})
+
+
+setValidity2("TapestriExperiment", function(object) {
+    msg <- NULL
+
+    if (length(grnaProbe(object)) != 1) {
+        msg <- c(msg, "'grnaProbe' should have length equal to 1")
+    }
+    if (length(barcodeProbe(object)) != 1) {
+        msg <- c(
+            msg, "'barcodeProbe' should have length equal to 1"
+        )
+    }
+
+    if (length(msg)) {
+        msg
+    } else TRUE
+})
