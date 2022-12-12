09ff191645f52ec3d95f5c6117ec59abef590817
Joey Mays <joeymays94@gmail.com>
2022-12-12 12:07:05 -0500

    added normalizeCounts

---
+++ R/normalization.R
@@ -0,0 +1,58 @@
+#' Normalize Raw Counts
+#'
+#' Normalizes raw counts from `counts` slot in a TapestriExperiment and returns the object with normalized counts in the `normcounts` slot.
+#' "mb" is the only method supported currently.
+#' This method performs the same normalization scheme as in Mission Bio's mosaic package for python.
+#' Counts for each barcode are normalized relative to their barcode means and probe counts are normalized relative to their probe medians.
+#' Also calculates the standard deviation for each amplicon using normalized counts and adds it to `rowData`.
+#'
+#' @param TapestriExperiment TapestriExperiment object
+#' @param method Chr string indicating normalization method. Default "mb".
+#'
+#' @return TapestriExperiment object with normalized counts added to `normcounts` slot.
+#' @export
+#'
+#' @importFrom stats median sd
+#'
+#' @examples
+#' \dontrun{TapestriExperiment <- normalizeCounts(TapestriExperiment)}
+
+normalizeCounts <- function(TapestriExperiment, method = "mb"){
+
+    method <- tolower(method)
+
+    if(method != "mb"){
+        message("Only method 'mb' is currently supported. Running as `method = 'mb'.")
+    }
+
+    raw.count.matrix <- SummarizedExperiment::assay(TapestriExperiment, "counts")
+
+    # get "good barcodes", barcodes that have at least 10% the counts of the 11th barcode ranked for highest number of counts
+    barcode.sums <- apply(raw.count.matrix, MARGIN = 2, sum)
+    barcode.sorted <- sort(barcode.sums, decreasing = T)
+    good.barcodes <- barcode.sums > (barcode.sorted[11] / 10)
+
+    # normalize barcodes relative to barcode means
+    barcode.means <- apply(raw.count.matrix, 2, mean) + 1
+    read.counts.normal <- sweep(raw.count.matrix, 2, barcode.means, "/")
+
+    # normalize probes relative to probe median. medians calculated using "good barcodes"
+    probe.medians <- apply(read.counts.normal[,good.barcodes], 1, median) + 0.05
+    read.counts.normal <- sweep(read.counts.normal, 1, probe.medians, "/")
+
+    # scale all counts by 2X for diploid baseline
+    read.counts.normal <- read.counts.normal * 2
+
+    # add to normalized counts slot
+    normcounts(TapestriExperiment) <- read.counts.normal
+
+    # calculate norm count SD for each amplicon and add to rowData
+    current.probe.data <- SummarizedExperiment::rowData(TapestriExperiment)
+    current.probe.order <- rownames(current.probe.data)
+    new.probe.data <- apply(read.counts.normal, 1, sd)
+    names(new.probe.data) <- rownames(read.counts.normal)
+    new.probe.data <- new.probe.data[current.probe.order]
+    SummarizedExperiment::rowData(TapestriExperiment)$norm.count.sd <-  new.probe.data
+
+    return(TapestriExperiment)
+}
--- data-raw/test-objects.R
+++ data-raw/test-objects.R
@@ -40,21 +40,33 @@ reducedDimPlot(tap2, "umap", group.label = "cluster")
 reducedDimPlot(tap2, "umap", group.label = "sample.grna")
 
 
-
+library(tidyverse)
 tap.file <- "~/Documents/data-and-analyses/tapestri/data/Exp003-20220808-CO293/run20220808-02-panelCO293-ref20220419.dna.h5"
 bam.file <- "~/Documents/data-and-analyses/tapestri/data/Exp003-20220808-CO293/run20220808-02-panelCO293-ref20220419.tube1.cells.bam"
 barcode.lookup <- data.frame(ids = c("g7x1", "gNC", "g9p21L2", "g9p21R2", "g7x3"), sequences = c("TGGATATATGGACCGCATTG", "ACGGAGGCTAAGCGTCGCAA", "TATTTACAGGGACAATACCG", "CGGTAGAATAAGCTGTACCG", "ACTCTTGCTGTGGCATTTTC"))
 coldata.labels <- c("g7x1", "gNC", "g9p21L2", "g9p21R2", "g7x3")
 
-tap <- createTapestriExperiment(tap.file, "CO293")
-tap <- parseBarcodedReads(tap, bam.file = bam.file, barcode.lookup = barcode.lookup, probe.tag = "grna")
-tap <- callSampleLables(tap, coldata.labels = c("g7x3", "gNC"), sample.label = "sample.grna")
-tap <- runPCA(tap, sd.min.threshold = 40)
-PCAKneePlot(tap)
-tap <- runUMAP(tap, input.dims = 1:2)
-reducedDimPlot(tap, "umap")
-tap <- getClusters(tap, eps = 0.9)
-reducedDimPlot(tap, "umap", group.label = "cluster")
-reducedDimPlot(tap, "umap", group.label = "sample.grna")
+exp3 <- createTapestriExperiment(tap.file, "CO293")
+exp3 <- parseBarcodedReads(exp3, bam.file = bam.file, barcode.lookup = barcode.lookup, probe.tag = "grna")
+exp3 <- callSampleLables(exp3, coldata.labels = c("g7x3", "gNC"), sample.label = "sample.grna")
+exp3 <- runPCA(exp3, sd.min.threshold = 40)
+PCAKneePlot(exp3)
+exp3 <- runUMAP(exp3, input.dims = 1:2)
+reducedDimPlot(exp3, "umap")
+exp3 <- getClusters(exp3, eps = 0.9)
+reducedDimPlot(exp3, "umap", group.label = "cluster")
+reducedDimPlot(exp3, "umap", group.label = "sample.grna")
+fct_count(colData(exp3)$cluster)
+exp3.subset <- exp3[,colData(exp3)$cluster %in% 1:3]
+reducedDimPlot(exp3.subset, "umap", group.label = "cluster")
+colData(exp3.subset)$cluster <- fct_recode(colData(exp3.subset)$cluster, RPE = "1", L1 = "2", L2 = "3")
+colData(exp3.subset)$cluster <- fct_drop(colData(exp3.subset)$cluster)
+fct_count(colData(exp3.subset)$cluster)
+exp3.subset <- exp3.subset[rowData(exp3.subset)$median.reads > 0,] #0 filtered for exp 3
+exp3.subset <- normalizeCounts(exp3.subset)
+
+# automatically filter out control medians == 0?
+
+
 
 
